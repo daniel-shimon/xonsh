ff5112d64fbab72c55b6ef86860a653b6cc0c606
Daniel Shimon <daniel.shimon22@gmail.com>
2020-12-12 12:09:04 +0200

    parsers: base: Refactor out a ``raise_parse_error`` function

--- xonsh/parsers/base.py
+++ xonsh/parsers/base.py
@@ -7,6 +7,7 @@ import textwrap
 from threading import Thread
 from ast import parse as pyparse
 from collections.abc import Iterable, Sequence, Mapping
+from typing import List, Optional, Union, Tuple
 
 from xonsh.ply.ply import yacc
 
@@ -204,6 +205,29 @@ def hasglobstar(x):
             return False
     else:
         return False
+
+
+def raise_parse_error(
+    msg: Union[str, Tuple[str]],
+    loc: Optional[Location] = None,
+    code: Optional[str] = None,
+    lines: Optional[List[str]] = None,
+):
+    if loc is None or code is None or lines is None:
+        err_line_pointer = ""
+    else:
+        col = loc.column + 1
+        if loc.lineno == 0:
+            loc.lineno = len(lines)
+        i = loc.lineno - 1
+        if 0 <= i < len(lines):
+            err_line = lines[i].rstrip()
+            err_line_pointer = "\n{}\n{: >{}}".format(err_line, "^", col)
+        else:
+            err_line_pointer = ""
+    err = SyntaxError("{0}: {1}{2}".format(loc, msg, err_line_pointer))
+    err.loc = loc  # type: ignore
+    raise err
 
 
 class YaccLoader(Thread):
@@ -621,22 +645,7 @@ class BaseParser(object):
         raise SyntaxError()
 
     def _parse_error(self, msg, loc):
-        if self.xonsh_code is None or loc is None:
-            err_line_pointer = ""
-        else:
-            col = loc.column + 1
-            lines = self.lines
-            if loc.lineno == 0:
-                loc.lineno = len(lines)
-            i = loc.lineno - 1
-            if 0 <= i < len(lines):
-                err_line = lines[i].rstrip()
-                err_line_pointer = "\n{}\n{: >{}}".format(err_line, "^", col)
-            else:
-                err_line_pointer = ""
-        err = SyntaxError("{0}: {1}{2}".format(loc, msg, err_line_pointer))
-        err.loc = loc
-        raise err
+        raise_parse_error(msg, loc, self.xonsh_code, self.lines)
 
     #
     # Precedence of operators
